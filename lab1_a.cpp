6a0735ce9f32ac48394d5e7da566779d85b6a960
dungpham98 <phamtuandung12@gmail.com>
2022-09-15 11:27:34 +0700

    Update lab1_a.cpp

--- lab1_a.cpp
+++ lab1_a.cpp
@@ -22,6 +22,14 @@
 #include <gl/glut.h>
 
 /* -- DATA STRUCTURES ---------------------------------------------------- */
+class GLintPoint  {
+    public:
+    int x,y;
+    GLintPoint(int _x, int _y){
+        this->x = _x;
+        this->y = _y;
+    }
+};
 
 /* -- GLOBAL VARIABLES --------------------------------------------------- */
 
@@ -39,10 +47,11 @@
  * Returns     : void
  */
 
+
 void drawDot()  {
     glClear( GL_COLOR_BUFFER_BIT );
 	glColor3ub(255,0,0);
-    glPointSize(50.0f);
+    glPointSize(5.0f);
     glBegin(GL_POINTS);
     glColor3f(0.3, 0.3, 0.3);
     glVertex2i(300, 150);
@@ -51,6 +60,42 @@ void drawDot()  {
     glColor3f(0.5, 0.3, 0.7);
     glVertex2i(100, 350);
     glEnd();
+    glFinish();
+}
+
+void draw_rect(GLintPoint p1, GLintPoint p2, float color[]){
+    //int x1 = 100, y1 = 100, x2 = 300, y2 = 300;
+    glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+    glColor3f(color[0], color[1], color[2]); // Red
+    glVertex2i(p1.x, p2.y);     // Define vertices in counter-clockwise (CCW) order
+    glVertex2i(p2.x, p2.y);     //  so that the normal (front-face) is facing you
+    glVertex2i(p2.x, p1.y);
+    glVertex2i(p1.x, p1.y);
+    glEnd();
+}
+
+//TODO:
+// - Convert int to GLintPoint
+// - Finish draw triangle
+void draw_triange(GLintPoint p1, GLintPoint p2, GLintPoint p3, float color[]){
+    //int x1 = 100, y1 = 100, x2 = 300, y2 = 300;
+    glBegin(GL_POLYGON);              // Each set of 4 vertices form a quad
+    glColor3f(color[0], color[1], color[2]); // Red
+    glVertex2i(p1.x, p1.y);     // Define vertices in counter-clockwise (CCW) order
+    glVertex2i(p2.x, p2.y);     //  so that the normal (front-face) is facing you
+    glVertex2i(p3.x, p3.y);
+    glEnd();
+}
+
+void house_draw(){
+    float c1[3] = {1.0f, 0.0f, 0.0f};
+    float c2[3] = {0.0f, 1.0f, 0.0f};
+    float c3[3] = {0.0f, 0.0f, 1.0f};
+    glClear( GL_COLOR_BUFFER_BIT );
+    GLintPoint p1(300,300), p2(100,100), p3(250,250),p4(200,200), p5(50,300), p6(200, 350), p7(350, 300);
+    draw_rect(p1, p2, c1);
+    draw_rect(p3, p4, c2);
+    draw_triange(p5,p6,p7, c3);
     glFinish();
 }
 
@@ -115,7 +160,7 @@ int main( int argc, char *argv[] )  {
   // Create the window.
   glutCreateWindow( "Lab" );
   // Set the callback funcion to call when we need to draw something.
-  glutDisplayFunc( drawDot );
+  glutDisplayFunc( drawDot); // Choose between drawDot, draw_rect or draw_house
   // Initialize some things.
   myInit( );
   // Now that we have set everything up, loop responding to events.
